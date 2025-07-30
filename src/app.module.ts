a819f33afabb331570c15306520ea1daa09ff380
codecinn27 <codecinnpro@gmail.com>
2025-07-30 11:24:29 +0800

    added graphQL configuration with basic User template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql', //code first approach, generate the GraphQL schema into this file
+      debug: true,
+      playground: true, // enable localhost:3000/graphql browser environment to test the query
+    }),
+
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
